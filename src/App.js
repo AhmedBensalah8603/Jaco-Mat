b7fdf49dd6759b11c73ea487e09f64c9c114c533
ahmed_bensalah <150929393+AhmedBensalah8603@users.noreply.github.com>
2025-02-26 21:25:28 +0000

    new0

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './auth.js'; // Import du contexte d'authentification
+import { AuthProvider } from './auth.js'; // Contexte d'authentification
 import HomePage from './HomePage';
-import Options from './options'; // Utiliser une majuscule pour Options
+import Options from './options'; // Correction : Majuscule pour Options
 import MatrixApp from './MatrixApp';
 import Interface from './interface';
 import Filee from './file';
 import About from './aboutus';
-import SignPage from './sign'; // Import du composant pour "Create Account"
-import LogPage from './log.js';   // Import du composant pour "Login"
+import SignPage from './sign'; // Page de création de compte
+import LogPage from './log.js'; // Page de connexion
 
 function App() {
   return (
     <AuthProvider>
-      <Router>
+      {/* Ajout du basename pour GitHub Pages */}
+      <Router basename="/Jaco-Mat">
         <Routes>
-          {/* Route principale */}
           <Route path="/" element={<HomePage />} />
-          
-          {/* Autres routes existantes */}
           <Route path="/options" element={<Options />} />
           <Route path="/aboutus" element={<About />} />
-          <Route path="/matrix" element={<MatrixApp />} /> {/* Matrice avec la modale */}
+          <Route path="/matrix" element={<MatrixApp />} />
           <Route path="/interface" element={<Interface />} />
           <Route path="/file" element={<Filee />} />
-
-          {/* Routes pour créer un compte et se connecter */}
           <Route path="/create-account" element={<SignPage />} />
           <Route path="/login" element={<LogPage />} />
         </Routes>
